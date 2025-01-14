8bc8ea9b372c3af72c90a483c695d50468b4ca3e
Arpan-gl <arpang754@gmail.com>
2025-01-14 13:38:46 +0530

    implement authentication with Redux; add Header and Footer components; set up store configuration

---
+++ Blog-App-Frontend/src/store/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+
+const store = configureStore({
+    reducer: authReducer, // Add your reducers here
+});
+
+export default store;
